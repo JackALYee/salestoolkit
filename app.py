3317a514ff06dcbd4beb31a51f277709e8420de1
JackALYee <jackiealyee@gmail.com>
2026-03-02 09:44:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -891,7 +891,7 @@ else:
                 <span class="gradient-text">North America</span><br>
                 <span style="font-weight: 300;">Trucking Division</span>
             </h1>
-            <div class="header-meta fade-up">Version 1.0 • Trucking BU • Jan 2026</div>
+            <div class="header-meta fade-up">Version BETA 1.7 • Trucking BU • Mar 2026</div>
         </div>
     </header>
 
